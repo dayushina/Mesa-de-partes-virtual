cb5c8eed6ad39d5b2ed1ca5373b378623646cccf
dayushina <dayamaushinahua@gmail.com>
2025-05-19 14:59:41 -0500

    correccion

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <meta charset="utf-8">
         <title>Login | Minia - Minimal Admin & Dashboard Template</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+        <meta content="Sistema de mesa de partes del programa de estudiso-IESTP" name="description">
         <meta content="Themesbrand" name="author">
 
         <!-- preloader css -->
